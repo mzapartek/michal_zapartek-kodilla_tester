fb23ceb04b3ba92e22b7d39c0d84e485e7ce7b2b
Michał Zapartek <zapartek@wp.pl>
2026-07-08 20:15:33 +0200

    Practice variables

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Michal";
+        int myAge = 30;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}
