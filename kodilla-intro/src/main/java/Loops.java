ff4248803f2a6905a3337499c5c21f2c52e0308f
Michał Zapartek <zapartek@wp.pl>
2026-07-14 14:26:06 +0200

    Dodaj cwiczenie z petla for

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+
+    public int sumNumbers(int[] numbers) {
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result = result + numbers[i];
+        }
+
+        return result;
+    }
+}
