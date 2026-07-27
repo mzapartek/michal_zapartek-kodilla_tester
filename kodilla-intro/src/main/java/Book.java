554749cd7127f216dae117f8017e58a33cd8336c
Michał Zapartek <zapartek@wp.pl>
2026-07-27 22:24:40 +0200

    Zadanie 2.7 - zmienne i metody statyczne

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    private String author;
+    private String title;
+
+    private Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+}
